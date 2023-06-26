008cffce483adb32562f8a1e5342aa0ae4ec0623
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-06-26 00:46:34 -1000

    Add hero section with styles and animations

---
+++ src/modules/Hero.js
@@ -0,0 +1,17 @@
+import "./styles/hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero_container">
+      <div className="hero--text_container">
+        <h1 className="hero--text1">
+          <span className="hero--text">Join the</span>
+        </h1>
+        <h1 className="hero--text2">
+          <span className="hero--text">NPC Revolution</span>
+        </h1>
+        <h3 className="hero--subtext">Shop these Original NPC bestsellers!</h3>
+      </div>
+    </div>
+  );
+}
