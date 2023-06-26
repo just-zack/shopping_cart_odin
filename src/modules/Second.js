bcdad36e05dd21fb3a7498a8ebe56b8f2499e384
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-06-26 13:26:36 -1000

    Adjust Second module styles

--- src/modules/Second.js
+++ src/modules/Second.js
@@ -5,8 +5,18 @@ import "./styles/second.css";
 export default function Second() {
   return (
     <div className="second_container">
-      <img className="second--img1" src={img1}></img>
-      <img className="second--img2" src={img2}></img>
+      <div className="second--img_container">
+        <img
+          className="second--img1"
+          src={img1}
+          alt="woman standing in a white NPC branded tshirt"
+        ></img>
+        <img
+          className="second--img2"
+          src={img2}
+          alt="woman standing in a black NPC branded tshirt"
+        ></img>
+      </div>
     </div>
   );
 }
