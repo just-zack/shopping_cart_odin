5166d7439b62c5d3903f6e673861013be1090fcb
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-06-28 13:58:57 -1000

    Start styling shop pg

--- src/Shop.js
+++ src/Shop.js
@@ -5,32 +5,38 @@ import Footer from "./modules/footer";
 import Modal from "./modules/modal";
 import ItemCard from "./modules/ItemCard";
 
+import "./modules/styles/shop_pg.css";
+
 export default function Shop({ openModal, setOpenModal, color, setColor }) {
   return (
-    <div className="shop_container">
+    <div className="shopPG_container">
       <NavbarStatic
         openModal={openModal}
         setOpenModal={setOpenModal}
         color={color}
         setColor={setColor}
       />
-      <div className="shop-text_container">
-        <h1 className="shop--title">SHOP NPC</h1>
-        <h3 className="shop--description">JOIN THE NPC REVOLUTION</h3>
-      </div>
-      <div className="shop--card_container">
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+      <div className="shop--container">
+        <div className="shop-text_container">
+          <div className="shop--title_container">
+            <h1 className="shop--title">SHOP NPC</h1>
+          </div>
+          <h3 className="shop--description">JOIN THE NPC REVOLUTION</h3>
+        </div>
+        <div className="shop--card_container">
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </div>
       </div>
       <Footer />
       {openModal && (
